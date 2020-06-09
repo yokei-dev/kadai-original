b54e1a6a57b4ffd575918829f821681334753e7d
taro.kirameki <yokei.gomi@gmail.com>
2020-06-09 00:34:17 +0000

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email,password)
+      flash[:success] = 'ログインに成功しました。'
+      redirect_to @user
+    else
+      flash[:danger] = 'ログインに失敗しました。'
+      render :new
+    end
+  end
+  
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+    flash[:success] = 'ログアウトしました。'
+  end
+  
+  
+  private
+  
+  def login(email,password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+  
+  
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  
+  before_action :require_user_logged_in, only: [:index, :show]
+  
+  def index
+    @users = User.all.order(id: :desc).page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+      flash[:success] = 'ユーザー登録をしました。'
+    else
+      render :new
+      flash[:danger] = 'ユーザー登録に失敗しました。'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+      flash[:success] = 'ユーザーを更新しました。'
+    else
+      render :edit
+      flash[:danger] = 'ユーザー更新に失敗しました。'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'ユーザーを削除しました。'
+    redirect_to root_url
+  end
+  
+  private
+  
+  
+  
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
+end
