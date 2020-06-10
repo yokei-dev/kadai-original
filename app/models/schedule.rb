360eeecc497e1c058432e2d83eb0855af28dd2b6
taro.kirameki <yokei.gomi@gmail.com>
2020-06-10 11:43:25 +0000

    finish4

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,11 +3,18 @@ class Schedule < ApplicationRecord
   validates :event, presence: true, length: { maximum: 255 }
   validates :departure_time, presence: true
   validates :arrival_time, presence: true
+  validates :price, numericality: {less_than: 10000000}
   
   validate :departure_time_earlier_than_arrival_time
+  validate :not_before_today
   
+   
   def departure_time_earlier_than_arrival_time
     errors.add(:arrival_time, '出発時間よりも到着時間の方を遅くにしてください') if departure_time && arrival_time &&  departure_time > arrival_time
+  end
+  
+  def not_before_today
+    errors.add(:date, '今日以降の日程を設定してください') if date < Date.today
   end
   
   
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -9,8 +9,15 @@ class Trip < ApplicationRecord
   validates :started_at, presence: true
   validates :finished_at, presence: true
   validate :started_at_earlier_than_finished_at
+  validate :not_before_today
+  
   
   def started_at_earlier_than_finished_at
     errors.add(:finished_at, '旅行開始日よりも旅行終了日の方を遅くにしてください') if started_at > finished_at
   end
+  
+  def not_before_today
+    errors.add(:started_at, '今日以降の日程を設定してください') if started_at < Date.today
+  end
+  
 end
